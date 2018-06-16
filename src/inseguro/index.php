fc9af39f5ebd3f3ebd0546ce816a2cc9d43e7e1e
KrishnaXavier <tsixav@gmail.com>
2018-06-16 10:40:00 -0300

    Estrtura login

--- src/inseguro/index.php
+++ src/inseguro/index.php
@@ -8,10 +8,10 @@ require_once "inc.header.php";
 	<div class="card card-container">		
 		<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
 		<p id="profile-name" class="profile-name-card"></p>
-		<form class="form-signin">
+		<form class="form-signin" action="form.login.php" method="post">
 			<span id="reauth-email" class="reauth-email"></span>
-			<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-			<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>			
+			<input name="inputEmail" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+			<input name="inputPassword" type="password" id="inputPassword" class="form-control" placeholder="Password" required>			
 			<button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
 		</form>		
 	</div>
